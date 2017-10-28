a656bee460c9b15f2634e763adac975cd95eb388
Kryllik <v.maxime@gmail.com>
2017-10-28 13:46:11 +0200

    création de l'array dans Board

--- Board.h
+++ Board.h
@@ -1,9 +1,13 @@
 //Header de Board.h
 
+#include "Cell.h"
+
 class Board{
 	public:
 		Board(); 
 		~Board();
+		static constexpr int BOARD_SIZE = 8; //Taille standard d'un plateau de jeu
 	private:
+		Cell array[BOARD_SIZE][BOARD_SIZE];
 		void initBoard();
 };
